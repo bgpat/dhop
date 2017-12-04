0f4a4c4c69b0136a6d6c326a6e4b5bb9e191f8e3
bgpat <bgpat@users.noreply.github.com>
2017-12-04 11:16:44 +0900

    Add unmarshal global method

--- dhcpop.go
+++ dhcpop.go
@@ -61,3 +61,65 @@ func Decode(code byte, b []byte) (Option, error) {
 		return o, err
 	}
 }
+
+func Unmarshal(code byte, b []byte) (Option, error) {
+	switch code {
+	case 0:
+		o := new(Padding)
+		err := o.Unmarshal(b)
+		return o, err
+	case 1, 16, 28, 32, 50, 54, 78, 95:
+		o := new(IPv4)
+		err := o.Unmarshal(b)
+		return o, err
+	case 2:
+		o := new(TimeOffset)
+		err := o.Unmarshal(b)
+		return o, err
+	case 3, 4, 5, 6, 7, 8, 9, 10, 11, 41, 42, 44, 45, 48, 49, 65,
+		68, 69, 70, 71, 72, 73, 74, 75, 76, 92, 112, 118, 138, 150:
+		o := new(IPv4s)
+		err := o.Unmarshal(b)
+		return o, err
+	case 13, 22, 26, 57, 93:
+		o := new(Size)
+		err := o.Unmarshal(b)
+		return o, err
+	case 19, 20, 27, 29, 30, 31, 34, 36, 39:
+		o := new(Boolean)
+		err := o.Unmarshal(b)
+		return o, err
+	case 21, 33:
+		o := new(IPv4Pair)
+		err := o.Unmarshal(b)
+		return o, err
+	case 23, 37, 46, 52, 53, 116:
+		o := new(Byte)
+		err := o.Unmarshal(b)
+		return o, err
+	case 24, 35, 38, 51, 58, 59, 91:
+		o := new(TimeDuration)
+		err := o.Unmarshal(b)
+		return o, err
+	case 25:
+		o := new(Sizes)
+		err := o.Unmarshal(b)
+		return o, err
+	case 121, 249:
+		o := new(Routes)
+		err := o.Unmarshal(b)
+		return o, err
+	case 119:
+		o := new(DomainNames)
+		err := o.Unmarshal(b)
+		return o, err
+	case 255:
+		o := new(End)
+		err := o.Unmarshal(b)
+		return o, err
+	default:
+		o := new(String)
+		err := o.Unmarshal(b)
+		return o, err
+	}
+}
